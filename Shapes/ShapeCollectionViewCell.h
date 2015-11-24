846f60404bbb9ad3942eb8a59b3a9956ed93bb9a
Anca Julean <anca.julean@softvision.ro>
2015-11-24 17:31:27 +0200

    first commit

---
+++ Shapes/ShapeCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ShapeCollectionViewCell.h
+//  Shapes
+//
+//  Created by Anca Julean on 24/11/15.
+//  Copyright © 2015 Anca Julean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShapeCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Shapes/ShapeCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ShapeCollectionViewCell.m
+//  Shapes
+//
+//  Created by Anca Julean on 24/11/15.
+//  Copyright © 2015 Anca Julean. All rights reserved.
+//
+
+#import "ShapeCollectionViewCell.h"
+
+@implementation ShapeCollectionViewCell
+
+@end
