66e8a0e98d7a769eda977673cc495ff8bf4f7a36
Anca Julean <anca.julean@softvision.ro>
2015-11-24 18:04:09 +0200

    added food shape type

--- Shapes/ViewController.m
+++ Shapes/ViewController.m
@@ -13,6 +13,8 @@
 
 @property (nonatomic, strong) NSArray *shapesArray;
 @property (nonatomic, assign) NSInteger selectedIndex;
+@property (nonatomic, assign) NSInteger selectedShapeTypeIndex;
+@property (nonatomic, strong) NSArray *currentShapes;
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (weak, nonatomic) IBOutlet UITableView *menuTableView;
@@ -25,8 +27,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.shapesArray = @[@"🐶", @"🐱", @"🐭", @"🐰", @"🐻", @"🦁", @"🐮", @"🐷", @"🐸", @"🐔", @"🐣", @"🐝", @"🐛", @"🐌", @"🐞", @"🐜", @"🐟", @"🐘", @"🐫", @"🐑"];
-    self.title = @"Animals";
+    self.shapesArray = @[@{@"Type":@"Animals",
+                           @"Shapes":@[@"🐶", @"🐱", @"🐭", @"🐰", @"🐻", @"🦁", @"🐮", @"🐷", @"🐸", @"🐔", @"🐣", @"🐝", @"🐛", @"🐌", @"🐞", @"🐜", @"🐟", @"🐘", @"🐫", @"🐑"]},
+                         @{@"Type":@"Food",
+                           @"Shapes":@[@"🍎", @"🍐", @"🍊" ,@"🍋", @"🍌", @"🍉", @"🍇", @"🍓", @"🍒", @"🍍"]}];
     
     self.collectionView.pagingEnabled = YES;
     
@@ -37,23 +41,33 @@
     [self.collectionView setCollectionViewLayout:flowLayout];
     
     self.menuHeightConstraint.constant = 0.0;
+    
+    self.selectedShapeTypeIndex = 0;
+    
+    self.title = ((NSDictionary *)self.shapesArray[self.selectedShapeTypeIndex])[@"Type"];
+    self.currentShapes = ((NSDictionary *)self.shapesArray[self.selectedShapeTypeIndex])[@"Shapes"];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self.collectionView reloadData];
+}
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return [self.shapesArray count];
+    return [self.currentShapes count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     ShapeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ShapeCollectionViewCell" forIndexPath:indexPath];
     
-    cell.shapeLabel.text = self.shapesArray[indexPath.row];
+    cell.shapeLabel.text = self.currentShapes[indexPath.row];
     
     return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return [self.shapesArray count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -63,9 +77,28 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"OptionCell"];
     }
     
-    cell.textLabel.text = @"Smth";
+    cell.textLabel.text = ((NSDictionary *)self.shapesArray[indexPath.row])[@"Type"];
     
     return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    self.selectedShapeTypeIndex = indexPath.row;
+    [self menuButtonAction:nil];
+}
+
+- (void)setSelectedShapeTypeIndex:(NSInteger)selectedShapeTypeIndex {
+    if (_selectedShapeTypeIndex != selectedShapeTypeIndex) {
+        _selectedShapeTypeIndex = selectedShapeTypeIndex;
+        
+        self.title = ((NSDictionary *)self.shapesArray[self.selectedShapeTypeIndex])[@"Type"];
+        self.currentShapes = ((NSDictionary *)self.shapesArray[self.selectedShapeTypeIndex])[@"Shapes"];
+        
+        [self.menuTableView reloadData];
+        [self.collectionView reloadData];
+    }
 }
 
 - (IBAction)menuButtonAction:(id)sender {
